66fa4090e3b36cd336233e8b5d3001659ed48515
Henryk Konsek <hekonsek@gmail.com>
2015-05-25 10:28:12 +0200

    Cloud deploy script allows now to choose between UI and services deployments.

--- iot/cloudlet/scripts/deploy-cloud.sh
+++ iot/cloudlet/scripts/deploy-cloud.sh
@@ -15,13 +15,30 @@
 
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+then
+  export DEPLOY_CLOUDLETS=true
+  export DEPLOY_UI=true
+else
+  export "${1}"=true
+fi
+
 if [ -z "$CLOUD_SSH_ROOT" ]
 then
   echo '$CLOUD_SSH_ROOT not set.'
   exit 1
 fi
 
-ssh $CLOUD_SSH_ROOT 'mkdir -p /var/camel-iot-labs/cloudlets'
-ssh $CLOUD_SSH_ROOT 'mkdir -p /var/camel-iot-labs/cloudlets-ui'
-scp ../geofencing/default/target/camel-labs-iot-cloudlet-geofencing-0.0.0-SNAPSHOT.jar $CLOUD_SSH_ROOT:/var/camel-iot-labs/cloudlets
-scp ././../../cloudlet-ui/geofencing-ui/target/camel-labs-iot-cloudlet-ui-geofencing-0.0.0-SNAPSHOT-plugin.zip $CLOUD_SSH_ROOT:/var/camel-iot-labs/cloudlets-ui
+if [ ! -z "$DEPLOY_CLOUDLETS" ]
+then
+  echo Deploying cloudlets UI.
+  ssh $CLOUD_SSH_ROOT 'mkdir -p /var/camel-iot-labs/cloudlets'
+  scp ../geofencing/default/target/camel-labs-iot-cloudlet-geofencing-0.0.0-SNAPSHOT.jar $CLOUD_SSH_ROOT:/var/camel-iot-labs/cloudlets
+fi
+
+if [ ! -z "$DEPLOY_UI" ]
+then
+  echo Deploying cloudlets UI.
+  ssh $CLOUD_SSH_ROOT 'mkdir -p /var/camel-iot-labs/cloudlets-ui'
+  scp ././../../cloudlet-ui/geofencing-ui/target/camel-labs-iot-cloudlet-ui-geofencing-0.0.0-SNAPSHOT-plugin.zip $CLOUD_SSH_ROOT:/var/camel-iot-labs/cloudlets-ui
+fi
