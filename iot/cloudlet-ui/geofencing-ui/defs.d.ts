0a1e4039e452422d0a79ce23a317b79b8f892176
Henryk Konsek <hekonsek@gmail.com>
2015-06-20 15:24:18 +0200

    Added basic Jasmine tests

--- iot/cloudlet-ui/geofencing-ui/defs.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="d.ts/includes.d.ts"/>
-/// <reference path="d.ts/example/ts/exampleGlobals.d.ts"/>
-/// <reference path="d.ts/example/ts/examplePlugin.d.ts"/>
-/// <reference path="d.ts/example/ts/routes.d.ts"/>
