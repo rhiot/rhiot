3e7922fe9c9b22c41fd8e6ac4a5529e32d3ccc16
Henryk Konsek <hekonsek@gmail.com>
2015-06-20 16:56:51 +0200

    Added basic Jasmine tests

--- iot/cloudlet-ui/geofencing-ui/d.ts/example/ts/exampleGlobals.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../includes.d.ts" />
-declare module Example {
-    var pluginName: string;
-    var log: Logging.Logger;
-    var templatePath: string;
-    function geofencingCloudletApiBase(): string;
-    function cloudletApiBase(): string;
-    function uriParam(name: any): string;
-}
