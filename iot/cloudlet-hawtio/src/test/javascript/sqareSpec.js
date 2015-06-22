5886e85e000fa2df3d1f4efd1e13ecd148e2ef9a
Henryk Konsek <hekonsek@gmail.com>
2015-06-22 22:36:00 +0200

    Fixed licenses and broken tests.

--- iot/cloudlet-hawtio/src/test/javascript/sqareSpec.js
+++ iot/cloudlet-hawtio/src/test/javascript/sqareSpec.js
@@ -15,27 +15,7 @@
  * limitations under the License.
  */
 
-module.exports = function(config) {
-    config.set({
-        basePath: '../../..',
-        frameworks: ['jasmine'],
-        files: [
-            'libs/angular/angular.js',
-            'libs/angular-route/angular-route.js',
-            'libs/angular-mocks/angular-mocks.js',
-            'plugins/example/ts/geofencing.js',
-            'src/test/javascript/*.js'
-        ],
-        exclude: ['src/test/javascript/karma.conf*.js'],
-        reporters: ['progress'],
-        port: 9876,
-        logLevel: config.LOG_INFO,
-        browsers: ['PhantomJS'],
-        singleRun: false,
-        autoWatch: true,
-        plugins: [
-            'karma-jasmine',
-            'karma-phantomjs-launcher'
-        ]
+describe('TODO', function () {
+    it('TODO', function () {
     });
-};
+});
