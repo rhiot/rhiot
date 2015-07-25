b4bb6bc72fb914527979adbfa4d1a73b4759997d
Henryk Konsek <hekonsek@gmail.com>
2015-07-25 10:16:33 +0200

    Migrated mock sensor to use Vert.x triggers.

--- iot/gateway/src/main/groovy/com/github/camellabs/iot/gateway/mock/MockSensorVerticle.groovy
+++ iot/gateway/src/main/groovy/com/github/camellabs/iot/gateway/mock/MockSensorVerticle.groovy
@@ -1,10 +1,26 @@
+/**
+ * Licensed to the Camel Labs under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.camellabs.iot.gateway.mock
 
 import com.github.camellabs.iot.gateway.GatewayVerticle
 import com.github.camellabs.iot.vertx.camel.GroovyCamelVerticle
-import org.apache.camel.builder.RouteBuilder
 
 import static com.github.camellabs.iot.vertx.PropertyResolver.intProperty
+import static java.util.UUID.randomUUID
 
 @GatewayVerticle(conditionProperty = 'camellabs_iot_gateway_mock_sensor')
 class MockSensorVerticle extends GroovyCamelVerticle {
@@ -15,17 +31,12 @@ class MockSensorVerticle extends GroovyCamelVerticle {
 
     @Override
     void start() {
-        camelContext.addRoutes(new RouteBuilder() {
-            @Override
-            void configure()  {
-                for (int i = 1; i <= sourcesNumber; i++) {
-                    String sourceId = "mockSensor-" + i;
-                    from("timer:" + sourceId + "?period=${period}").routeId(sourceId).
-                            transform().groovy("UUID.randomUUID().toString()").
-                            to("seda:mockSensor?blockWhenFull=true");
-                }
+        def producer = camelContext.createProducerTemplate()
+        for (int i = 1; i <= sourcesNumber; i++) {
+            vertx.setPeriodic(period) {
+                producer.sendBody("seda:mockSensor?blockWhenFull=true", randomUUID().toString())
             }
-        })
+        }
     }
 
 }
