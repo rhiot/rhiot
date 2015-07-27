48de1e871ecfff863881e2b8b33eadfc1d2a1517
Henryk Konsek <hekonsek@gmail.com>
2015-07-27 20:07:22 +0200

    Added JavaCamelVerticle test.

---
+++ iot/gateway-app/src/test/java/com/github/camellabs/iot/gateway/app/CustomJavaCamelVerticleTest.java
@@ -0,0 +1,48 @@
+package com.github.camellabs.iot.gateway.app;
+
+import com.github.camellabs.iot.gateway.GatewayVerticle;
+import com.github.camellabs.iot.gateway.VertxGateway;
+import com.github.camellabs.iot.vertx.camel.JavaCamelVerticle;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.github.camellabs.iot.vertx.camel.CamelContextFactories.camelContext;
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+public class CustomJavaCamelVerticleTest {
+
+    static VertxGateway vertxGateway = new VertxGateway();
+
+    @BeforeClass
+    public static void beforeClass() {
+        vertxGateway.start();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        vertxGateway.stop();
+    }
+
+    @Test
+    public void shouldReceiveHeartbeat() throws InterruptedException {
+        // Given
+        MockEndpoint mockEndpoint = camelContext().getEndpoint("mock:customJava", MockEndpoint.class);
+        mockEndpoint.setMinimumExpectedMessageCount(1);
+
+        // Then
+        MockEndpoint.assertIsSatisfied(1, MINUTES, mockEndpoint);
+    }
+
+}
+
+@GatewayVerticle
+class CustomJavaHeartbeatConsumerVerticle extends JavaCamelVerticle {
+
+    @Override
+    public void start() throws Exception {
+        fromEventBus("heartbeat", route -> route.to("mock:customJava"));
+    }
+
+}
