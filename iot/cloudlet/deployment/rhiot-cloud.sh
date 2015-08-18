645a10c10abd8325ce7ebacc830b9f941753ad92
Henryk Konsek <hekonsek@gmail.com>
2015-08-18 10:20:34 +0200

    Fixed Rhiot Cloud script.

--- iot/cloudlet/deployment/rhiot-cloud.sh
+++ iot/cloudlet/deployment/rhiot-cloud.sh
@@ -19,14 +19,14 @@
 
 echo Checking Docker setup...
 
-if ! type "$foobar_command_name" > /dev/null; then
+if ! type "docker" > /dev/null; then
   echo 'Docker not found - installing...'
   wget -qO- https://get.docker.com/ | sh
 fi
 
 REQUIRED_DOCKER_VERSION=1.7.1
 DOCKER_VERSION=`docker version | grep 'Server version' | cut -d ' ' -f 3`
-if [[ "$DOCKER_VERSION" < "$REQUIRED_DOCKER_VERSION" ]]; then
+if [ "$DOCKER_VERSION" \< "$REQUIRED_DOCKER_VERSION" ]; then
   echo "Docker ${REQUIRED_DOCKER_VERSION} is required to run Rhiot Cloud. Version ${DOCKER_VERSION} found - upgrading..."
   wget -qO- https://get.docker.com/ | sh
 fi
@@ -39,7 +39,10 @@ service docker start
 
 docker stop $(docker ps -q)
 
+docker rm mongodb
+docker run -d --name mongodb -p 27017:27017 mongo
+
 docker run -d -p 9000:9000 -e LIVE_RELOAD=false \
   -e GOOGLE_OAUTH_CLIENT_ID=$GOOGLE_OAUTH_CLIENT_ID -e GOOGLE_OAUTH_CLIENT_SECRET=$GOOGLE_OAUTH_CLIENT_SECRET \
-  -eGOOGLE_OAUTH_REDIRECT_URI=$GOOGLE_REDIRECT_URI \
+  -e GOOGLE_OAUTH_REDIRECT_URI=$GOOGLE_OAUTH_REDIRECT_URI \
   rhiot/cloudlet-console
