e312c0a17f8a4bc151f9bdce1f64d52897c45972
Henryk Konsek <hekonsek@gmail.com>
2015-08-28 14:46:11 +0200

    Added authorization CORS header to ODocument REST API.

--- iot/cloudlet/document/default/src/main/java/com/github/camellabs/iot/cloudlet/document/driver/DriverDocumentCloudlet.java
+++ iot/cloudlet/document/default/src/main/java/com/github/camellabs/iot/cloudlet/document/driver/DriverDocumentCloudlet.java
@@ -110,7 +110,7 @@ public class DriverDocumentCloudlet extends FatJarRouter {
                 response.setHeader("Access-Control-Allow-Origin", "*");
                 response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
                 response.setHeader("Access-Control-Max-Age", "3600");
-                response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+                response.setHeader("Access-Control-Allow-Headers", "Authorization, x-requested-with");
                 chain.doFilter(req, res);
             }
 
