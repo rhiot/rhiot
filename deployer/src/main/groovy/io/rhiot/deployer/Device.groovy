69312cae791e6dfafb6da83419726404672acb57
Henryk Konsek <hekonsek@gmail.com>
2015-09-28 17:43:14 +0200

    Added toString.

--- deployer/src/main/groovy/io/rhiot/deployer/Device.groovy
+++ deployer/src/main/groovy/io/rhiot/deployer/Device.groovy
@@ -16,13 +16,16 @@
  */
 package io.rhiot.deployer
 
+import groovy.transform.ToString
+
+@ToString
 public class Device {
 
-    public static final String DEVICE_RASPBERRY_PI_2 = "RaspberryPi2";
+    static final String DEVICE_RASPBERRY_PI_2 = "RaspberryPi2"
 
-    private final InetAddress address;
+    private final InetAddress address
 
-    private final String type;
+    private final String type
 
     public Device(InetAddress address, String type) {
         this.address = address;
