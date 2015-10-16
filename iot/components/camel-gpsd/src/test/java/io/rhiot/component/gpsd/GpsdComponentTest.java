05c2d47ab1fd17cd4a88e988509e3a01f53b7769
Taariq Levack <taariql@gmail.com>
2015-10-16 07:10:51 +0200

    Consuming TPVObject should handle exception with provided handler

--- iot/components/camel-gpsd/src/test/java/io/rhiot/component/gpsd/GpsdComponentTest.java
+++ iot/components/camel-gpsd/src/test/java/io/rhiot/component/gpsd/GpsdComponentTest.java
@@ -20,37 +20,58 @@ package io.rhiot.component.gpsd;
 import de.taimos.gpsd4java.backend.GPSdEndpoint;
 import de.taimos.gpsd4java.types.PollObject;
 import de.taimos.gpsd4java.types.TPVObject;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.spi.ExceptionHandler;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static java.util.Collections.singletonList;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public class GpsdComponentTest extends CamelTestSupport {
 
+    private static final CountDownLatch LATCH = new CountDownLatch(1);
+
     GPSdEndpoint gpsd = mock(GPSdEndpoint.class);
+    GPSdEndpoint failingGpsd = mock(GPSdEndpoint.class);
 
     @Before
     public void before() throws IOException {
         PollObject pollObject = new PollObject();
         pollObject.setFixes(singletonList(new TPVObject()));
         given(gpsd.poll()).willReturn(pollObject);
+        given(failingGpsd.poll()).willThrow(new IllegalArgumentException("Forced"));
     }
 
     @Test
     public void smokeTest() throws Exception {
         Thread.sleep(2000);
     }
+    
+    @Test
+    public void testException() throws Exception{
+
+        getMockEndpoint("mock:result").expectedMessageCount(0);
+
+        assertMockEndpointsSatisfied();
+
+        assertTrue("Should have been called", LATCH.await(5, TimeUnit.SECONDS));
+    }
 
     @Override
     protected JndiRegistry createRegistry() throws Exception {
         JndiRegistry registry = super.createRegistry();
         registry.bind("gpsd4javaEndpoint", gpsd);
+        registry.bind("failingGpsdEndpoint", failingGpsd);
+        registry.bind("myHandler", new MyExceptionHandler());
         return registry;
     }
 
@@ -59,8 +80,30 @@ public class GpsdComponentTest extends CamelTestSupport {
         return new RouteBuilder() {
             public void configure() {
                 from("gpsd://gps?scheduled=true&restartGpsd=false&gpsd4javaEndpoint=#gpsd4javaEndpoint").to("seda:mock");
+
+                //Set router up for failure
+                from("gpsd:failingGps?scheduled=true&restartGpsd=false&gpsd4javaEndpoint=#failingGpsdEndpoint&consumer.exceptionHandler=#myHandler").routeId("gps").
+                        throwException(new IllegalArgumentException("Forced")).to("mock:result");
             }
         };
+    }
+
+    private final class MyExceptionHandler implements ExceptionHandler {
+
+        @Override
+        public void handleException(Throwable exception) {
+            LATCH.countDown();
+        }
+
+        @Override
+        public void handleException(String message, Throwable exception) {
+            LATCH.countDown();
+        }
+
+        @Override
+        public void handleException(String message, Exchange exchange, Throwable exception) {
+            LATCH.countDown();
+        }
     }
 
 }
