ad24ef28d31d29ea0fdc0f9d449f2b1b2e93c64e
Henryk Konsek <hekonsek@gmail.com>
2015-10-27 17:18:55 +0100

    Create Spark boot initializer.

---
+++ steroids-spark/src/main/groovy/io/rhiot/steroids/spark/SparkBootInitializer.groovy
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Rhiot under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.rhiot.steroids.spark
+
+import io.rhiot.steroids.bootstrap.BootInitializer
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+
+class SparkBootInitializer implements BootInitializer {
+
+    private JavaSparkContext sparkContext
+
+    @Override
+    void start() {
+        def conf = new SparkConf().setAppName("Rhiot application").setMaster('local[*]')
+        sparkContext = new JavaSparkContext(conf);
+    }
+
+    @Override
+    void stop() {
+        sparkContext.stop()
+    }
+
+    @Override
+    int order() {
+        2000
+    }
+
+    JavaSparkContext sparkContext() {
+        sparkContext
+    }
+
+}
