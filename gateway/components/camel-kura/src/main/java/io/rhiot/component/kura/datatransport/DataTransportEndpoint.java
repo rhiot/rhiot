93d44a5a7a5426ece19a27264ffd8255fddcee3c
Henryk Konsek <hekonsek@gmail.com>
2015-12-23 14:33:13 +0100

    Created Kura DataTransport component.

---
+++ gateway/components/camel-kura/src/main/java/io/rhiot/component/kura/datatransport/DataTransportEndpoint.java
@@ -0,0 +1,88 @@
+/**
+ * Licensed to the Rhiot under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.rhiot.component.kura.datatransport;
+
+import org.apache.camel.Component;
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.impl.DefaultEndpoint;
+import org.eclipse.kura.data.DataTransportService;
+
+public class DataTransportEndpoint extends DefaultEndpoint {
+
+    private DataTransportService dataTransportService;
+
+    private String topic;
+
+    private int qos = 0;
+
+    private boolean retain = false;
+
+    public DataTransportEndpoint(String endpointUri, Component component, String topic) {
+        super(endpointUri, component);
+        this.topic = topic;
+    }
+
+    @Override
+    public Producer createProducer() throws Exception {
+        return new DataTransportProducer(this);
+    }
+
+    @Override
+    public Consumer createConsumer(Processor processor) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public DataTransportService getDataTransportService() {
+        return dataTransportService;
+    }
+
+    public void setDataTransportService(DataTransportService dataTransportService) {
+        this.dataTransportService = dataTransportService;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public int getQos() {
+        return qos;
+    }
+
+    public void setQos(int qos) {
+        this.qos = qos;
+    }
+
+    public boolean isRetain() {
+        return retain;
+    }
+
+    public void setRetain(boolean retain) {
+        this.retain = retain;
+    }
+
+}
---
+++ gateway/components/camel-kura/src/main/java/io/rhiot/component/kura/datatransport/DataTransportProducer.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Rhiot under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.rhiot.component.kura.datatransport;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultProducer;
+import org.eclipse.kura.data.DataTransportService;
+
+import static io.rhiot.component.kura.datatransport.DataTransportConstants.CAMEL_KURA_DATATRANSPORT_TOPIC;
+
+public class DataTransportProducer extends DefaultProducer {
+
+    public DataTransportProducer(DataTransportEndpoint endpoint) {
+        super(endpoint);
+    }
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        byte[] payload = exchange.getIn().getBody(byte[].class);
+        String topic = exchange.getIn().getHeader(CAMEL_KURA_DATATRANSPORT_TOPIC, String.class);
+        if(topic == null) {
+            topic = getEndpoint().getTopic();
+        }
+
+        DataTransportService dataTransportService = getEndpoint().getDataTransportService();
+        dataTransportService.publish(topic, payload, getEndpoint().getQos(), getEndpoint().isRetain());
+    }
+
+    @Override
+    public DataTransportEndpoint getEndpoint() {
+        return (DataTransportEndpoint) super.getEndpoint();
+    }
+
+}
