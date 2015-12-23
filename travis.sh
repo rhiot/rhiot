c0290dd50d5af133cb8723e0c64c39ffa6f288e2
Henryk Konsek <hekonsek@gmail.com>
2015-12-23 12:13:00 +0100

    Fixed broken Travis script.

--- travis.sh
+++ travis.sh
@@ -41,10 +41,10 @@ trap 'error_handler' ERR
 bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
-if [ -e "${DEPLOY}" ]; then
-    mvn clean deploy -DskipTests --settings ~/.m2/mySettings.xml >> $BUILD_OUTPUT 2>&1
-else
+if [ -z "${DEPLOY}" ]; then
     mvn install -PwithRatCheck >> $BUILD_OUTPUT 2>&1
+else
+    mvn clean deploy -DskipTests --settings ~/.m2/mySettings.xml >> $BUILD_OUTPUT 2>&1
 fi
 
 # The build finished without returning an error so dump a tail of the output
