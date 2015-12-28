cde54e1dd7a72f5e28d00664717c8ded39112a60
gautric <gautric@redhat.com>
2015-12-28 09:33:20 +0100

    change kura to rhiot

--- dockerfiles/rpi-rhiot/start_kura_rhiot.sh
+++ dockerfiles/rpi-rhiot/start_kura_rhiot.sh
@@ -18,15 +18,15 @@
 #!/bin/sh
 
 export KURA_HTTP_PORT=${KURA_HTTP_PORT:-80}
-export KURA_PLUGIN_FOLDER=${KURA_PLUGIN_FOLDER:-/opt/rhiot/plugins}
+export RHIOT_PLUGIN_FOLDER=${RHIOT_PLUGIN_FOLDER:-/opt/rhiot/plugins}
 
 cd /opt/eclipse/kura/kura
 
 #sed -i "" -e "s/.*org.osgi.service.http.port.*=.*\$/org.osgi.service.http.port=${KURA_HTTP_PORT}/" "config.ini"
 
-if [ -d ${KURA_PLUGIN_FOLDER} ]; then
-  for i in `ls ${KURA_PLUGIN_FOLDER} | egrep "*.jar"`; do
-    cat config.ini | awk '/^osgi.bundles=/ { print $0 ", reference\\:file\\:'"${KURA_PLUGIN_FOLDER}/${i}"'@5\\:start" }   !/osgi.bundles=/ { print $0  }' > config.ini.tmp
+if [ -d ${RHIOT_PLUGIN_FOLDER} ]; then
+  for i in `ls ${RHIOT_PLUGIN_FOLDER} | egrep "*.jar"`; do
+    cat config.ini | awk '/^osgi.bundles=/ { print $0 ", reference\\:file\\:'"${RHIOT_PLUGIN_FOLDER}/${i}"'@5\\:start" }   !/osgi.bundles=/ { print $0  }' > config.ini.tmp
     mv config.ini.tmp config.ini
     # FIX ME sed -i "" -e "s/osgi.bundles=.*/&1, reference\\\:file\\\:${i}@5\\\:start/" "config.ini"
   done
