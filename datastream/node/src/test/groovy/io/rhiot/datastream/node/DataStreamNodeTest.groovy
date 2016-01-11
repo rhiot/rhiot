5610fd8d703324edae45a8e63e4959b322a659a2
Henryk Konsek <hekonsek@gmail.com>
2016-01-11 21:17:00 +0100

    Removed Spark stream from node.

--- datastream/node/src/test/groovy/io/rhiot/datastream/node/DataStreamNodeTest.groovy
+++ datastream/node/src/test/groovy/io/rhiot/datastream/node/DataStreamNodeTest.groovy
@@ -16,64 +16,20 @@
  */
 package io.rhiot.datastream.node
 
-import com.mongodb.BasicDBObject
-import com.mongodb.Mongo
 import io.rhiot.cloudplatform.test.DataStreamTest
-import io.rhiot.mongodb.EmbeddedMongo
-import io.vertx.core.json.Json
-import org.apache.camel.component.spark.RddCallback
-import org.apache.spark.api.java.AbstractJavaRDDLike
-import org.apache.spark.api.java.JavaPairRDD
-import org.apache.spark.api.java.JavaRDD
-import org.apache.spark.api.java.JavaSparkContext
-import org.junit.AfterClass
 import org.junit.Test
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 import static com.google.common.truth.Truth.assertThat
-import static org.apache.camel.component.spark.SparkMongos.mongoRdd
 
 @Configuration
 class DataStreamNodeTest extends DataStreamTest {
-
-    static def mongo = new EmbeddedMongo().start()
-
-    @AfterClass
-    static void afterClass() {
-        mongo.stop()
-    }
 
     @Test
     void smokeTestMongoDocumentStreamConsumer() {
         fromBus('document.save.doc', [foo: 'bar'], String.class)
         def count = fromBus('document.count.doc', int.class)
         assertThat(count).isEqualTo(1)
-    }
-
-    @Test
-    void smokeTestMongoSparkTask() {
-        def mongoClient = new Mongo('localhost', mongo.port())
-        mongoClient.getDB('db').getCollection('collection').save(new BasicDBObject([foo: 'bar']))
-
-        def encodedResult = producerTemplate.requestBody("amqp:spark.execute.rdd.callback", Json.encode([payload: 10]), String.class)
-        def result = Json.decodeValue(encodedResult, Map.class).payload
-        assertThat(result).isEqualTo(10)
-    }
-
-    @Bean
-    RddCallback callback() {
-        new RddCallback<Long>() {
-            @Override
-            Long onRdd(AbstractJavaRDDLike rdd, Object... payloads) {
-                rdd.count() * (int) payloads[0]
-            }
-        }
-    }
-
-    @Bean
-    JavaPairRDD rdd(JavaSparkContext sparkContext) {
-        mongoRdd(sparkContext, 'localhost', mongo.port(), 'db', 'collection')
     }
 
 }
