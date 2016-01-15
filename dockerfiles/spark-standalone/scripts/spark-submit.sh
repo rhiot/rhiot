f87832087cd39f9c4a5b8068fc3386069e2f1409
Henryk Konsek <hekonsek@gmail.com>
2016-01-15 20:18:06 +0100

    Spark should submit in cluster mode by default.

--- dockerfiles/spark-standalone/scripts/spark-submit.sh
+++ dockerfiles/spark-standalone/scripts/spark-submit.sh
@@ -17,4 +17,4 @@
 
 export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 cd /opt/spark
-bin/spark-submit --master spark://${SPARK_MASTER_SERVICE_HOST}:7077 --conf spark.driver.host=${SPARK_LOCAL_IP} "$@"
+bin/spark-submit --master spark://${SPARK_MASTER_SERVICE_HOST}:7077 --deploy-mode cluster --conf spark.driver.host=${SPARK_LOCAL_IP} "$@"
