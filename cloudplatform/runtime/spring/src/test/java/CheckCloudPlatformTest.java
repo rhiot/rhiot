4dc125d268190a1704fe2f33ab3f16096750c21b
gautric <gautric@redhat.com>
2016-01-21 13:13:21 +0100

    add void TestClass for CloudPlatformTest

---
+++ cloudplatform/runtime/spring/src/test/java/CheckCloudPlatformTest.java
@@ -0,0 +1,15 @@
+import io.rhiot.cloudplatform.runtime.spring.test.CloudPlatformTest;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CheckCloudPlatformTest extends CloudPlatformTest {
+
+    @Test
+    @Ignore
+    public void testCloudPlaformTestClassTest() {
+        assertTrue(true);
+    }
+}
