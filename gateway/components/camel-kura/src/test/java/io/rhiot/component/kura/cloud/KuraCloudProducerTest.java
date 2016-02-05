44c4a12f428104ee0dc3682bb41e7c811ffcf510
gautric <gautric@redhat.com>
2016-02-05 21:02:55 +0100

    [rhiot#493]KuraCloudProducer doesn't manage correctly includeDeviceId prop
    To confirm error

--- gateway/components/camel-kura/src/test/java/io/rhiot/component/kura/cloud/KuraCloudProducerTest.java
+++ gateway/components/camel-kura/src/test/java/io/rhiot/component/kura/cloud/KuraCloudProducerTest.java
@@ -16,16 +16,6 @@
  */
 package io.rhiot.component.kura.cloud;
 
-import com.google.common.truth.Truth;
-import org.apache.camel.impl.JndiRegistry;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.eclipse.kura.KuraException;
-import org.eclipse.kura.cloud.CloudClient;
-import org.eclipse.kura.cloud.CloudService;
-import org.eclipse.kura.message.KuraPayload;
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyInt;
@@ -34,21 +24,37 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.eclipse.kura.KuraException;
+import org.eclipse.kura.cloud.CloudClient;
+import org.eclipse.kura.cloud.CloudService;
+import org.eclipse.kura.message.KuraPayload;
+import org.eclipse.kura.system.SystemService;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.truth.Truth;
+
 public class KuraCloudProducerTest extends CamelTestSupport {
 
     CloudService cloudService = mock(CloudService.class);
 
     CloudClient cloudClient = mock(CloudClient.class);
 
+    SystemService systemService = mock(SystemService.class);
+
     @Before
     public void before() throws KuraException {
         given(cloudService.newCloudClient(anyString())).willReturn(cloudClient);
+        given(systemService.getSerialNumber()).willReturn("SerialNumber-XYZ");
     }
 
     @Override
     protected JndiRegistry createRegistry() throws Exception {
         JndiRegistry registry = super.createRegistry();
         registry.bind("cloudService", cloudService);
+        registry.bind("systemService", systemService);
         return registry;
     }
 
@@ -65,9 +71,23 @@ public class KuraCloudProducerTest extends CamelTestSupport {
     }
 
     @Test
+    public void shouldSendKuraPayloadToTopicAddDeviceId() throws KuraException {
+        // Given
+        KuraPayload kuraPayload = new KuraPayload();
+
+        // When
+        template.sendBody("kura-cloud:app/topic?includeDeviceId=true", kuraPayload);
+
+        // Then
+        verify(systemService).getSerialNumber();
+    }
+
+    @Test
     public void shouldReuseCacheClient() throws Exception {
-        CloudClient firstCloudClient = context.getEndpoint("kura-cloud:app/topic1", KuraCloudEndpoint.class).createProducer().cloudClient;
-        CloudClient secondCloudClient = context.getEndpoint("kura-cloud:app/topic2", KuraCloudEndpoint.class).createProducer().cloudClient;
+        CloudClient firstCloudClient = context.getEndpoint("kura-cloud:app/topic1", KuraCloudEndpoint.class)
+                .createProducer().cloudClient;
+        CloudClient secondCloudClient = context.getEndpoint("kura-cloud:app/topic2", KuraCloudEndpoint.class)
+                .createProducer().cloudClient;
         Truth.assertThat(firstCloudClient).isSameAs(secondCloudClient);
     }
 
