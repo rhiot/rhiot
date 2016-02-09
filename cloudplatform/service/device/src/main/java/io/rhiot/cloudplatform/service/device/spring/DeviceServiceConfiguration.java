51ec70a6ef3a6e4644d1d80263d08da29c5f9121
Henryk Konsek <hekonsek@gmail.com>
2016-02-09 13:26:04 +0100

    Device service components should be overridable.

--- cloudplatform/service/device/src/main/java/io/rhiot/cloudplatform/service/device/spring/DeviceServiceConfiguration.java
+++ cloudplatform/service/device/src/main/java/io/rhiot/cloudplatform/service/device/spring/DeviceServiceConfiguration.java
@@ -22,17 +22,20 @@ import io.rhiot.cloudplatform.service.binding.ServiceBinding;
 import io.rhiot.cloudplatform.service.device.MongoDbDeviceRegistry;
 import org.eclipse.cloudplatform.service.device.api.DeviceRegistry;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DeviceServiceConfiguration {
 
+    @ConditionalOnBean
     @Bean
     ServiceBinding deviceServiceBinding(PayloadEncoding payloadEncoding) {
         return new ServiceBinding(payloadEncoding, "device");
     }
 
+    @ConditionalOnBean
     @Bean(name = "device")
     DeviceRegistry deviceRegistry(Mongo mongo,
                                   @Value("${device.metrics.mongodb.db:rhiot}") String db,
