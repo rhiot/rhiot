818b0dc604cfb2b11f7f8d648ddcdbe6ededdcbe
Henryk Konsek <hekonsek@gmail.com>
2016-02-22 09:57:44 +0100

    Fixing concurrent consumers.

--- cloudplatform/service/binding/src/main/java/io/rhiot/cloudplatform/service/binding/ServiceBinding.java
+++ cloudplatform/service/binding/src/main/java/io/rhiot/cloudplatform/service/binding/ServiceBinding.java
@@ -57,30 +57,32 @@ public class ServiceBinding extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        String fromChannel = format("amqp:%s.>?replyToConcurrentConsumers=20", serviceChannel);
-        LOG.debug("Starting route consuming from channel: {}", fromChannel);
+        for(int i = 0; i < 20; i++) {
+            String fromChannel = format("amqp:%s.>", serviceChannel);
+            LOG.debug("Starting route consuming from channel: {}", fromChannel);
 
-        from(fromChannel).process(exchange -> {
-            Message message = exchange.getIn();
-            String channel = message.getHeader("JMSDestination", Destination.class).getAddress();
-            byte[] incomingPayload = message.getBody(byte[].class);
-            OperationBinding operationBinding = operationBinding(payloadEncoding, channel, incomingPayload, message.getHeaders(), getContext().getRegistry());
-            exchange.setProperty(TARGET_PROPERTY, "bean:" + operationBinding.service() + "?method=" + operationBinding.operation() + "&multiParameterArray=true");
-            exchange.setProperty("RETURN_TYPE", operationBinding.operationMethod().getReturnType());
+            from(fromChannel).process(exchange -> {
+                Message message = exchange.getIn();
+                String channel = message.getHeader("JMSDestination", Destination.class).getAddress();
+                byte[] incomingPayload = message.getBody(byte[].class);
+                OperationBinding operationBinding = operationBinding(payloadEncoding, channel, incomingPayload, message.getHeaders(), getContext().getRegistry());
+                exchange.setProperty(TARGET_PROPERTY, "bean:" + operationBinding.service() + "?method=" + operationBinding.operation() + "&multiParameterArray=true");
+                exchange.setProperty("RETURN_TYPE", operationBinding.operationMethod().getReturnType());
 
-            message.setBody(new Camels().convert(getContext(), operationBinding.arguments(), operationBinding.operationMethod().getParameterTypes()));
-        }).toD(format("${property.%s}", TARGET_PROPERTY)).process(it -> {
-            Class returnType = it.getProperty("RETURN_TYPE", Class.class);
-            if(Void.TYPE == returnType) {
-                it.getIn().setBody(null);
-            }
-            if(byte[].class == returnType) {
-                it.getIn().setBody(it.getIn().getBody());
-                log.debug("Binary return type. No conversion performed.");
-            } else {
-                it.getIn().setBody(payloadEncoding.encode(it.getIn().getBody()));
-            }
-        });
+                message.setBody(new Camels().convert(getContext(), operationBinding.arguments(), operationBinding.operationMethod().getParameterTypes()));
+            }).toD(format("${property.%s}", TARGET_PROPERTY)).process(it -> {
+                Class returnType = it.getProperty("RETURN_TYPE", Class.class);
+                if (Void.TYPE == returnType) {
+                    it.getIn().setBody(null);
+                }
+                if (byte[].class == returnType) {
+                    it.getIn().setBody(it.getIn().getBody());
+                    log.debug("Binary return type. No conversion performed.");
+                } else {
+                    it.getIn().setBody(payloadEncoding.encode(it.getIn().getBody()));
+                }
+            });
+        }
     }
 
 }
