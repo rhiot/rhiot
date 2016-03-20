04956d28147cbdfe0d7782591338dd24984591ea
Henryk Konsek <hekonsek@gmail.com>
2016-03-20 21:11:29 +0100

    Added REST protocol adapter to Gateway PaaS.

--- cloudplatform/paas/gateway/src/test/groovy/io/rhiot/cloudplatform/paas/gateway/GatewayPaasTest.groovy
+++ cloudplatform/paas/gateway/src/test/groovy/io/rhiot/cloudplatform/paas/gateway/GatewayPaasTest.groovy
@@ -18,26 +18,39 @@ package io.rhiot.cloudplatform.paas.gateway
 
 import io.rhiot.cloudplatform.runtime.spring.test.CloudPlatformTest
 import io.rhiot.datastream.document.DocumentStore
+import org.apache.commons.io.IOUtils
 import org.junit.Test
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 import static com.google.common.truth.Truth.assertThat
 import static io.rhiot.cloudplatform.connector.Header.arguments
+import static io.rhiot.utils.Uuids.uuid
 import static org.mockito.Mockito.mock
 
 @Configuration
 class GatewayPaasTest extends CloudPlatformTest {
 
-    @Test
-    void shouldCountDocument() {
-        def count = connector.fromBus('document.count', long.class, arguments('someDocument'))
-        assertThat(count).isEqualTo(0)
-    }
+    def document = uuid()
 
     @Bean
     DocumentStore document() {
         mock(DocumentStore.class)
+    }
+
+    // Tests
+
+    @Test
+    void shouldCountDocument() {
+        def count = connector.fromBus('document.count', long.class, arguments(document))
+        assertThat(count).isEqualTo(0)
+    }
+
+    @Test
+    void shouldCountDocumentViaRestAdapter() {
+        def responseBytes = IOUtils.toByteArray(new URL("http://localhost:8080/document/count/${document}"))
+        def response = payloadEncoding.decode(responseBytes)
+        assertThat(response).isEqualTo(0)
     }
 
 }
