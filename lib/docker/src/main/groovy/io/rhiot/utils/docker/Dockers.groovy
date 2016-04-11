4494ccba6bef2da28fd7ea345a78c1c566165227
Henryk Konsek <hekonsek@gmail.com>
2016-04-11 10:16:58 +0200

    "Dockers" tool should not recreate existing containers #571

--- lib/docker/src/main/groovy/io/rhiot/utils/docker/Dockers.groovy
+++ lib/docker/src/main/groovy/io/rhiot/utils/docker/Dockers.groovy
@@ -23,11 +23,13 @@ import com.spotify.docker.client.DockerException
 import com.spotify.docker.client.ImageNotFoundException
 import com.spotify.docker.client.messages.ContainerConfig
 import com.spotify.docker.client.messages.ContainerCreation
+import com.spotify.docker.client.messages.HostConfig
+import com.spotify.docker.client.messages.PortBinding
 import groovy.transform.CompileStatic
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import static java.lang.Runtime.getRuntime
+import static com.spotify.docker.client.DockerClient.ListContainersParam.allContainers
 
 @CompileStatic
 public class Dockers {
@@ -59,20 +61,46 @@ public class Dockers {
         }
     }
 
-    String daemon(String image) {
-        def containerConfig = ContainerConfig.builder().image(image).build()
-        ContainerCreation containerCreation
-        try {
-            containerCreation = docker.createContainer(containerConfig)
-        } catch (ImageNotFoundException e) {
-            docker.pull(image)
-            containerCreation = docker.createContainer(containerConfig)
+    String daemon(String image, String name, int ... ports) {
+        def existingContainer = docker.listContainers(allContainers()).find{ it.names().contains('/' + name) }
+        if(existingContainer == null) {
+            def configBuilder = ContainerConfig.builder().image(image)
+            if (ports.length > 0) {
+                def portBindings = new HashMap<String, List<PortBinding>>()
+                ports.each {
+                    def hostPorts = new ArrayList<PortBinding>()
+                    hostPorts.add(PortBinding.of("0.0.0.0", '' + it));
+                    portBindings.put('' + it, hostPorts);
+                }
+                configBuilder.hostConfig(
+                        HostConfig.builder().portBindings(portBindings).build()
+                )
+            }
+            def containerConfig = configBuilder.build()
+            ContainerCreation containerCreation
+            try {
+                containerCreation = name != null ? docker.createContainer(containerConfig, name) : docker.createContainer(containerConfig)
+            } catch (ImageNotFoundException e) {
+                docker.pull(image)
+                containerCreation = docker.createContainer(containerConfig)
+            }
+            def id = containerCreation.id()
+            docker.startContainer(id)
+            registerShutdown(id)
+            id
+        } else if(existingContainer.status().startsWith('Up ')) {
+            LOG.debug('Container named {} exists. Skipping creation. ', name)
+            LOG.debug('Restarting container {}.', name)
+            docker.restartContainer(existingContainer.id())
+            registerShutdown(existingContainer.id())
+            existingContainer.id()
+        } else {
+            LOG.debug('Container named {} exists. Skipping creation.', name)
         }
-        def id = containerCreation.id()
-        docker.startContainer(id)
-        startedContainers.add(id)
-        registerShutdown(id)
-        id
+    }
+
+    String daemon(String image) {
+        daemon(image, null)
     }
 
     public void stop(String containerId) {
@@ -87,7 +115,8 @@ public class Dockers {
     }
 
     public void registerShutdown(String containerId) {
-        getRuntime().addShutdownHook(new Thread() {
+        startedContainers.add(containerId)
+        Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
                 try {
@@ -100,7 +129,8 @@ public class Dockers {
     }
 
     public static void main(String[] args) {
-        Dockers.dockers().daemon('mongo')
+        Dockers.dockers().daemon('mongo', 'poolxx', 27017)
+        Thread.sleep(10 * 1000)
     }
 
 }
